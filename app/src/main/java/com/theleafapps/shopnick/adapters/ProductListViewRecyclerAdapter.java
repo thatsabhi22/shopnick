9153c6b59dcb41dd2b80a3a44cdfd4c3f29e2aa3
The Aviator <abhi@xyz>
2016-05-06 03:41:01 +0530

    The staggered grid for the product list is working with dynamic network image and its mrp and off-price

--- app/src/main/java/com/theleafapps/shopnick/adapters/ProductListViewRecyclerAdapter.java
+++ app/src/main/java/com/theleafapps/shopnick/adapters/ProductListViewRecyclerAdapter.java
@@ -1,34 +1,75 @@
 package com.theleafapps.shopnick.adapters;
 
+import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.theleafapps.shopnick.R;
+import com.theleafapps.shopnick.models.Product;
+import com.theleafapps.shopnick.utils.MySingleton;
+
+import java.util.List;
 
 /**
  * Created by aviator on 26/04/16.
  */
 public class ProductListViewRecyclerAdapter extends RecyclerView.Adapter<ProductListViewRecyclerAdapter.ProductListViewHolder>{
 
+    Context mContext;
+    List<Product> productList;
+    private ImageLoader mImageLoader;
+
+    public ProductListViewRecyclerAdapter(Context context, List<Product> productList) {
+        this.productList = productList;
+        this.mContext = context;
+    }
+
     @Override
     public ProductListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_list_cards, null);
+        ProductListViewHolder rcv = new ProductListViewHolder(layoutView);
+        return rcv;
     }
 
     @Override
     public void onBindViewHolder(ProductListViewHolder holder, int position) {
 
+        mImageLoader = MySingleton.getInstance(mContext).getImageLoader();
+        holder.productName.setText(productList.get(position).product_name);
+        holder.productImage.setImageUrl(productList.get(position).image_url,mImageLoader);
+        holder.productMrp.setText("Rs " + String.valueOf(productList.get(position).unit_mrp));
+        holder.productMrp.setPaintFlags(holder.productMrp.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        holder.productOfferPrice.setText("Rs " + productList.get(position).unit_offerprice);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.productList.size();
     }
 
     public class ProductListViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView productName;
+        public NetworkImageView productImage;
+        public TextView productMrp;
+        public TextView productOfferPrice;
+
         public ProductListViewHolder(View itemView) {
             super(itemView);
+//            itemView.setOnClickListener(this);
+            productName         =   (TextView) itemView.findViewById(R.id.product_name);
+            productImage        =   (NetworkImageView) itemView.findViewById(R.id.product_image);
+            productMrp          =   (TextView) itemView.findViewById(R.id.price_mrp);
+            productOfferPrice   =   (TextView) itemView.findViewById(R.id.price_offer);
         }
+
     }
 
 }
--- app/src/main/java/com/theleafapps/shopnick/ui/ProductListActivity.java
+++ app/src/main/java/com/theleafapps/shopnick/ui/ProductListActivity.java
@@ -1,26 +1,63 @@
 package com.theleafapps.shopnick.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.theleafapps.shopnick.R;
+import com.theleafapps.shopnick.adapters.ProductListViewRecyclerAdapter;
+import com.theleafapps.shopnick.models.multiples.Products;
+import com.theleafapps.shopnick.tasks.GetProductsBySubCatIdTask;
+
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
 
 public class ProductListActivity extends AppCompatActivity {
 
+    Products productsRec;
     private StaggeredGridLayoutManager staggeredGridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
 
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_product_list);
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
+            RecyclerView recyclerView = (RecyclerView)findViewById(R.id.product_list_recycler_view);
+            recyclerView.setHasFixedSize(true);
 
-        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, 1);
-        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+            staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, 1);
+            recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+            Intent intent = getIntent();
+            int subCatId = Integer.valueOf(intent.getStringExtra("subCatId"));
+
+            if(subCatId > 0) {
+
+                GetProductsBySubCatIdTask getProductsBySubCatIdTask = new GetProductsBySubCatIdTask(this, subCatId);
+
+                boolean x = getProductsBySubCatIdTask.execute().get();
+
+                productsRec = getProductsBySubCatIdTask.productsRec;
+
+                if (productsRec != null && productsRec.products.size() > 0)
+                    productsRec.products.size();
+
+                    ProductListViewRecyclerAdapter rcAdapter = new ProductListViewRecyclerAdapter(ProductListActivity.this, productsRec.products);
+                    recyclerView.setAdapter(rcAdapter);
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
