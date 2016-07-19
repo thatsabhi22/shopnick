d7d7a8858739fae0625d1f5aa38c83e13c1b533c
The Aviator <abhi@xyz>
2016-07-19 19:09:55 +0530

    Designed checkout screen

--- app/src/main/java/com/theleafapps/shopnick/ui/CheckoutActivity.java
+++ app/src/main/java/com/theleafapps/shopnick/ui/CheckoutActivity.java
@@ -1,15 +1,31 @@
 package com.theleafapps.shopnick.ui;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.theleafapps.shopnick.R;
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    ActionBar actionBar;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_checkout);
+        setSupportActionBar(toolbar);
+
+        actionBar =  getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Complete Your Order");
+
+
+
     }
 }
